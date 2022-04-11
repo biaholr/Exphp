04798d2872d8b9977d84ab74f76a977415e29ace
biaholr <103462342+biaholr@users.noreply.github.com>
2022-04-11 19:07:01 -0300

    Add files via upload

---
+++ aula01.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Estamos aprendendo PHP!</h1>
+    <?php 
+        echo "Vamos proseguir aprendendo PHP";
+    ?>
+    <?php
+        echo "Oi, eu serei visto na sua tela";
+        //Eu não! Sou apenas um comentário.
+
+        echo "Oi, eu também serie visto por você";
+        # Já eu não serei! 
+
+        echo "E eu aqui novamente na sua tela, rs";
+        /*Eu não aparecerei na sua tela novamente 
+        pois sou um comentário */
+    ?>
+    
+</body>
+</html>
